dbc5a25b7cc80d8f9556af3fe88b8df9494e58f0
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-03-10 20:51:44 +0000

    heroku proxy

--- src/proxy.js
+++ src/proxy.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import fetch from 'node-fetch';
 
-import { getEarthquakes, setEarthquakes } from './cache.js';
+// import { getEarthquakes, setEarthquakes } from './cache.js';
 // import { fetchEarthquakes } from './earthquakes.js';
 
 export const router = express.Router();
@@ -16,53 +16,51 @@ router.get('/proxy', async (req, res) => {
   let result;
 
   // TODO skoða fyrst cachið
-  try {
-    result = await getEarthquakes(`${period}_${type}`);
-    // result = await fetchEarthquakes(period, type);
-    // eslint-disable-next-line no-console
-    console.log(result);
-  } catch (e) {
-    console.error('error getting from cache', e);
-  }
+  // try {
+  //   result = await getEarthquakes(`${period}_${type}`);
+  //   // result = await fetchEarthquakes(period, type);
+  //   // eslint-disable-next-line no-console
+  //   // console.log(result);
+  // } catch (e) {
+  //   console.error('error getting from cache', e);
+  // }
 
-  if (result) {
-    const dataman = {
-      data: JSON.parse(result),
-      info: {
-        cached: true,
-        time: 0.500,
-      },
-    };
-    res.json(dataman);
-    return;
-  }
+  // if (result) {
+  //   const dataman = {
+  //     data: JSON.parse(result),
+  //     info: {
+  //       cached: true,
+  //       time: 0.500,
+  //     },
+  //   };
+  //   res.json(dataman);
+  //   return;
+  // }
 
   try {
     result = await fetch(URL);
     // eslint-disable-next-line no-console
-    console.log(result);
+    // console.log(result);
   } catch (e) {
     console.error('Villa við að sækja gögn frá vefþjónustu', e);
     res.status(500).send('Villa við að sækja gögn frá vefþónustu');
-    return;
   }
 
   if (!result.ok) {
     console.error('Villa frá vefþjónustu', await result.text());
     res.status(500).send('Villa við að sækja gögn frá vefþjónustu');
-    return;
   }
 
   // TODO setja gögn í cache
-  const resultText = await result.text();
-  await setEarthquakes(`${period}_${type}`, resultText);
+  // const resultText = await result.text();
+  // await setEarthquakes(`${period}_${type}`, resultText);
 
-  const data = {
-    data: JSON.parse(resultText),
-    info: {
-      cached: false,
-      time: 0.500,
-    },
-  };
-  res.json(data);
+  // const data = {
+  //   data: JSON.parse(resultText),
+  //   info: {
+  //     cached: false,
+  //     time: 0.500,
+  //   },
+  // };
+  // res.json(data);
 });
